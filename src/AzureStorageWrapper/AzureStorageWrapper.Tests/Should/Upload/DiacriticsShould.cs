770b460223a68485cba57d4bc5ded82346a3599f
Sergio Barriel <sergiobarriel@gmail.com>
2025-03-06 17:08:11 +0100

    added azurite

--- src/AzureStorageWrapper/AzureStorageWrapper.Tests/Should/Upload/DiacriticsShould.cs
+++ src/AzureStorageWrapper/AzureStorageWrapper.Tests/Should/Upload/DiacriticsShould.cs
@@ -12,27 +12,7 @@ namespace AzureStorageWrapper.Tests.Should.Upload
             _azureStorageWrapper = azureStorageWrapper;
         }
 
-        [Fact]
-        public async Task UploadBlob_WithBlankSpacesInName_Should_UploadBlob()
-        {
-            var base64 = "SGVsbG8g8J+Zgg==";
-
-            var command = new UploadBase64()
-            {
-                Base64 = base64,
-                Container = "files",
-                Name = "hello world",
-                Extension = "md",
-                Metadata = new Dictionary<string, string>()
-                    {{"hello", "world"}}
-            };
-
-            var response = await _azureStorageWrapper.UploadBlobAsync(command);
-
-            Assert.NotNull(response);
-
-            Assert.True(await PingAsync(response.SasUri));
-        }
+     
 
         [Fact]
         public async Task UploadBlobWithDiacriticsInName_Should_UploadBlob()
@@ -52,9 +32,7 @@ namespace AzureStorageWrapper.Tests.Should.Upload
             var response = await _azureStorageWrapper.UploadBlobAsync(command);
 
             Assert.True(response.Name.IndexOfAny(new char[]{ 'á','é', 'í', 'ó', 'ú', 'ä', 'ë','ï', 'ö', 'ü', 'à', 'è', 'ì', 'ò', 'ù'}) > 0); // no diacritics
-
             Assert.NotNull(response);
-
             Assert.True(await PingAsync(response.SasUri));
         }
 
---
+++ src/AzureStorageWrapper/AzureStorageWrapper/AzureStorageWrapperBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AzureStorageWrapper
+{
+    public abstract class AzureStorageWrapperBase
+    {
+        /// <summary>
+        /// ~2 centuries of work are needed in order to have a 1% probability of at least one collision: https://alex7kom.github.io/nano-nanoid-cc
+        /// </summary>
+        internal string GetRandomId()
+        {
+            var guid = Guid.NewGuid().ToString("N");
+
+            return guid.Substring(0, 15);
+        }
+    }
+}
